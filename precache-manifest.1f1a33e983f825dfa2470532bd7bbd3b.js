964528163af7b43c7338b1d432f0064181497347
Rama Thakur <thakurc110@gmail.com>
2019-12-03 15:42:36 +0530

    Updates

---
+++ precache-manifest.1f1a33e983f825dfa2470532bd7bbd3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971ecdff3ee5e61f7d8ab7b0e60cf005",
+    "url": "/cartoonfriends/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/cartoonfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c9b7edbeb90034bd762c",
+    "url": "/cartoonfriends/static/css/main.0487444a.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/cartoonfriends/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "c9b7edbeb90034bd762c",
+    "url": "/cartoonfriends/static/js/main.cc6fc8db.chunk.js"
+  },
+  {
+    "revision": "31b2638edbfa7fd19b08",
+    "url": "/cartoonfriends/static/js/runtime-main.386219dc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/cartoonfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
